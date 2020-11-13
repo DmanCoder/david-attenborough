7c14e62863e239e992616987c7aa506e8c6afde4
DmanCoder <ktdenis.1@live.com>
2020-11-13 15:32:52 +1000

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import logo from './assets/logo/logo-white.png';
+
+import './styles/main.scss';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="loading"></div>
+      <div className="navigation">
+        <ul>
+          <li>
+            <img src={logo} alt="Logo" />
+          </li>
+          <li>
+            <span className="navigation__active">home</span>
+          </li>
+          <li>
+            <span>holiday</span>
+          </li>
+          <li>
+            <span>destinations</span>
+          </li>
+          <li>
+            <span>flights</span>
+          </li>
+          <li>
+            <span>offers</span>
+          </li>
+          <li>
+            <span>contacts</span>
+          </li>
+          <li>
+            <span className="material-icons">search</span>
+          </li>
+          <li>
+            <span className="material-icons">person</span>
+          </li>
+          <li>
+            <span className="material-icons">menu</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="banner"></div>
     </div>
   );
-}
+};
 
 export default App;
