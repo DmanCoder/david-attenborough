291bed5cacf4943326435a1fb59e74499c96f8d0
DmanCoder <ktdenis.1@live.com>
2020-11-20 10:19:12 +1000

    Setup poster position and image URL

---
+++ src/animations/helpers/imgURL.js
@@ -0,0 +1,17 @@
+// Assets
+import logo from '../../assets/logo/square.png';
+import dolphinsIMG from '../../assets/imgs/doliphins.jpg';
+import africaIMG from '../../assets/imgs/africa.jpg';
+import desertIMG from '../../assets/imgs/desert.jpg';
+import eagleIMG from '../../assets/imgs/eagle.jpg';
+import madagascarIMG from '../../assets/imgs/madagascar.jpg';
+import polarIMG from '../../assets/imgs/polar.jpg';
+
+export default {
+  dolphins: dolphinsIMG,
+  africa: africaIMG,
+  desert: desertIMG,
+  eagle: eagleIMG,
+  madagascar: madagascarIMG,
+  polar: polarIMG,
+};
--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { gsap, CSSRulePlugin } from './gsapInit';
 
 // Animations
 import loadingAnimation from './animations/loadingAnimation';
+import setUpAndPositionPoster from './animations/setUpAndPositionPoster';
 
 // Assets
 import logo from './assets/logo/square.png';
@@ -73,58 +74,7 @@ const moveFirstGalleryImageToEndOfGallery = (state) => {
 //   });
 // });
 
-const setUpAndPositionPoster = (state) => {
-  // Collect image gallery to array
-  const glItem = gsap.utils.toArray('.banner__gallery-item');
 
-  // Get rule
-  const posterAfter = CSSRulePlugin.getRule('.poster::after');
-
-  // Get the first index of the array
-  const glFirst = glItem[0];
-  const glLast = glItem[glItem.length - 1];
-  //
-  // Get x/y coordinates and width/height
-  const glFirstRect = glFirst.getBoundingClientRect();
-
-  //  Get data attribute
-  const subject = glFirst.dataset.subject;
-  const subjectLast = glLast.dataset.subject;
-
-  // Create background string
-  // const rgba = 'rgba(0, 0, 0, 0.3)';
-  // const bgLinear = `linear-gradient(${rgba}, ${rgba})`;
-  const bgURL = `url('${imgURL[subject]}') no-repeat center / cover`;
-  const bgURLLast = `url('${imgURL[subjectLast]}') no-repeat center / cover`;
-
-  // Position poster over above `glFirst`
-  gsap.set('.poster', {
-    css: {
-      background: bgURL,
-      top: glFirstRect.top,
-      left: glFirstRect.left,
-      width: glFirstRect.width,
-      height: glFirstRect.height,
-      borderRadius: '2rem',
-      boxShadow: '1.5rem 2rem 1rem rgba(0, 0, 0, 0.25)',
-      zIndex: '10',
-      // boxShadow: '15px 20px 10px rgba(0, 0, 0, 0.25)',
-    },
-  });
-
-  // Set background of `::after` to transparent
-  gsap.set(posterAfter, {
-    cssRule: { background: 'transparent' }, // Remove container mask
-  });
-  //
-  /* This section makes the `.poster` look like its the first item of the gallery */
-  // Add `.shadow-none` to first item in the gallery array
-  glFirst.classList.add('shadow-none');
-
-  // Hide background
-  gsap.set(glFirst, { css: { background: 'transparent' } });
-  gsap.set(glLast, { css: { background: bgURLLast } });
-};
 
 const posterExpansionAnimation = (state) => {
   // Collect image gallery to array
